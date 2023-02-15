b5a9557640c32269a5a184db8869bc99d41b9b30
Andrey_Marusov <marusov140@gmail.com>
2023-02-15 10:49:06 +0300

    Первый коммит тудушки

---
+++ src/scripts/script.js
@@ -0,0 +1,9 @@
+const input = document.querySelector('.form__input')
+const ul = document.querySelector('.todos__list')
+
+function createToDo() {
+    const li = document.createElement('li')
+    const span = document.createElement('span')
+
+
+}
