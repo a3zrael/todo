5e7af71ea6746f42f23fca7f28d325e7bfa5b622
Andrey_Marusov <marusov140@gmail.com>
2023-02-16 16:32:27 +0300

    Сделал динамичную дату, кнопнку удаления без функционала

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -4,47 +4,84 @@ const ul = document.querySelector('.todos__list')
 const tasks = JSON.parse(window.localStorage.getItem('task')) || []
 button.addEventListener('click', pushObj)
 
-
-
-
 const delBtn = document.createElement('button')
-delBtn.type = 'button';
-delBtn.innerHTML = 'Press me';
-delBtn.className = 'test__btn';
-
-var container = document.getElementById('container');
-container.appendChild(delBtn);
-
 
 function setData() {
     window.localStorage.setItem('task', JSON.stringify(tasks))
 }
 
+function setDate() {
+    let date = new Date()
+    let day = document.querySelector('.today');
+
+    function getWeekDay(date) {
+        let days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+        return days[date.getDay()]
+    }
+    function getMon(date) {
+        let month = ['Января', 'Февраля', 'Марта', 'Апреля', 'Майя', 'Июня', 'Июля', 'Августа',
+            'Сентября', 'Октября', 'Ноября', 'Декабря']
+        return month[date.getMonth()]
+    }
+    day.innerHTML = `${getWeekDay(date)}, ${date.getDate()} ${getMon(date)}`
+}
+
+function removeValue() {
+    input.value = ''
+}
+
+function removeTask() {
+
+}
+
 function createTask(task) {
     const li = document.createElement('li')
+    const text = document.createElement('span')
+    const btn = document.createElement('button')
+
     li.className = ('todos__item')
-    li.innerHTML = `${task.title}`;
+
+    text.innerHTML = `${task.title}`;
+
+    btn.innerHTML = 'Удалить'
+    btn.className = 'del__btn'
+    btn.addEventListener('click', () => {
+        removeTask()
+    })
+
+    li.append(text, btn)
+
     return li;
 }
 
 function pushObj() {
     let valueInput = input.value
+
     const task = {
         title: valueInput,
     }
+
     if (!valueInput) {
         return
     } else tasks.push(task);
-    setData()
-    let taskElem = createTask(task)
+
+    setData();
+
+    let taskElem = createTask(task);
+
     ul.prepend(taskElem);
-    console.log(tasks);
+
+    removeValue()
 }
 
 function renderTasks() {
     for (let i = 0; i < tasks.length; i++) {
+
         let taskElem = createTask(tasks[i])
+
         ul.prepend(taskElem);
     }
 }
 renderTasks()
+
+setDate()
