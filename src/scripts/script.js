f2d36b11e797dcd0a7b93db9d42d8cd7fc1beaf1
Andrey_Marusov <marusov140@gmail.com>
2023-02-16 14:38:46 +0300

    Инпут добавляет задачу через нажатие кнопки

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,9 +1,50 @@
 const input = document.querySelector('.form__input')
+const button = document.querySelector('.form__btn')
 const ul = document.querySelector('.todos__list')
-
-function createToDo() {
-    const li = document.createElement('li')
-    const span = document.createElement('span')
+const tasks = JSON.parse(window.localStorage.getItem('task')) || []
+button.addEventListener('click', pushObj)
 
 
+
+
+const delBtn = document.createElement('button')
+delBtn.type = 'button';
+delBtn.innerHTML = 'Press me';
+delBtn.className = 'test__btn';
+
+var container = document.getElementById('container');
+container.appendChild(delBtn);
+
+
+function setData() {
+    window.localStorage.setItem('task', JSON.stringify(tasks))
 }
+
+function createTask(task) {
+    const li = document.createElement('li')
+    li.className = ('todos__item')
+    li.innerHTML = `${task.title}`;
+    return li;
+}
+
+function pushObj() {
+    let valueInput = input.value
+    const task = {
+        title: valueInput,
+    }
+    if (!valueInput) {
+        return
+    } else tasks.push(task);
+    setData()
+    let taskElem = createTask(task)
+    ul.prepend(taskElem);
+    console.log(tasks);
+}
+
+function renderTasks() {
+    for (let i = 0; i < tasks.length; i++) {
+        let taskElem = createTask(tasks[i])
+        ul.prepend(taskElem);
+    }
+}
+renderTasks()
